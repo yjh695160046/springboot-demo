40278057d11cab4aa6d0fa53e33f63980a6ba38f
yaojinhua <123@qq.com>
2021-06-15 10:24:12 +0800

    init

---
+++ dubbo/src/main/java/com/yijian/dubbo/DubboApplication.java
@@ -0,0 +1,20 @@
+package com.yijian.dubbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: yaojinhua
+ * @date: 2021/6/15 10:20
+ * @description:
+ */
+@SpringBootApplication
+public class DubboApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(DubboApplication.class, args);
+
+
+    }
+}
---
+++ dubbo/src/test/java/com/yijian/dubbo/JunitTest.java
@@ -0,0 +1,9 @@
+package com.yijian.dubbo;
+
+/**
+ * @author: yaojinhua
+ * @date: 2021/6/15 10:23
+ * @description:
+ */
+public class JunitTest {
+}
